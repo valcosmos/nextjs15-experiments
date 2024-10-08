5718ea48f1b1f7f829f927180fd2dae41a0319ca
Cupid Valentine <westerngod@126.com>
2024-10-08 22:27:56 +0800

    feat: init the project

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import antfu from '@antfu/eslint-config'
+import nextPlugin from '@next/eslint-plugin-next'
+
+export default antfu({
+  react: true,
+  formatters: true,
+  plugins: { '@next/next': nextPlugin },
+  rules: {
+    ...nextPlugin.configs.recommended.rules,
+    ...nextPlugin.configs['core-web-vitals'].rules,
+    '@next/next/no-duplicate-head': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+  },
+})
